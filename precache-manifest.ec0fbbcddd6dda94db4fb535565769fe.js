c30c53c8666429dbaf36ea1b7c489f22f12f86d0
MelisaGramajo <melisargramajo@gmail.com>
2020-03-24 23:58:35 -0300

    Updates

---
+++ precache-manifest.ec0fbbcddd6dda94db4fb535565769fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "920cddd8ea347bcf87ae",
+    "url": "/portfolio-personal-melisa/static/css/main.20b7f7f7.chunk.css"
+  },
+  {
+    "revision": "920cddd8ea347bcf87ae",
+    "url": "/portfolio-personal-melisa/static/js/main.920cddd8.chunk.js"
+  },
+  {
+    "revision": "78e6f16d4706fe7c3388",
+    "url": "/portfolio-personal-melisa/static/js/runtime~main.78e6f16d.js"
+  },
+  {
+    "revision": "472f4e4bbab2f6ff5a53",
+    "url": "/portfolio-personal-melisa/static/js/2.472f4e4b.chunk.js"
+  },
+  {
+    "revision": "7922a0fea9166085c0d1566ceb95d55b",
+    "url": "/portfolio-personal-melisa/index.html"
+  }
+];
